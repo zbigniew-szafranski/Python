3e7b832bd4418410dd4b2843fb0bb6b21a15abef
ibhiz <zbigniew.szafranski@gmail.com>
2025-03-27 23:45:33 +0100

    rozw14

--- M03/zadanie09.py
+++ M03/zadanie09.py
@@ -1,7 +1,6 @@
 # Rozwiń program z M03L07. Tym razem format pliku jest następujący: każda linia to jeden wydatek. Najpierw jest kwota wydatku, a po spacji jego opis. Opis może zawierać kolejne spacje. Plik może zawierać puste linie. Przykładowy plik to expenses-ex.txt
 # W tym ćwiczeniu nie wystarczy, aby skorzystać ze .split(), dlatego że ono dzieli nie tylko na poszczególne linie, ale także po spacjach w każdej linii. Tutaj konieczne jest dzielenie tylko po znakach nowej linii. Jak to zrobić? Da się to zrobić używając .split(), ale trzeba przekazać pewien parametr. Doczytaj w dokumentacji jak dokładnie to zrobić.
 # Mając już pojedynczą linię konieczne będzie dalsze jej podzielenie już po spacjach, tak aby dostać się do kwoty wydatku, która jest na początku każdej linii.
-
 EXPENSES_FILENAME = 'M03/expenses_ex.txt'
 with open(EXPENSES_FILENAME) as stream:
     content = stream.read()
@@ -13,3 +12,4 @@ for line in lines:
         expense = token[0]
         total+=float(expense)
 print("Kwota wydatków: ", total)
+
---
+++ M03/zadanie14.py
@@ -0,0 +1,24 @@
+### 🔴 Ćwiczenie
+
+# Popraw kod z M03L09 tak, aby wczytać wydatki do listy, a następnie wylicz sumę i średnią wydatków, a także jakiej kwoty był najmniejszy i największy wydatek. Użyj wbudowanych funkcji sum, min oraz max.
+
+EXPENSES_FILENAME = 'M03/expenses_ex.txt'
+with open(EXPENSES_FILENAME) as stream:
+    content = stream.read()
+lines = content.split('\n')
+expenses = []
+# expenses_token = []
+for line in lines:
+    token=line.split()
+    if token:
+        expense = float(token[0])
+        expenses.append(expense)
+total: float = sum(expenses)
+minimum: float = min(expenses)
+maximum: float = max(expenses)
+avg: float = total / len(expenses)
+
+print("Suma wydatków to: ", total)
+print("Najmniejszy wydatek: ", minimum)
+print("Największy wydatek: ", maximum)
+print("Sredni wydatek: ", avg)
