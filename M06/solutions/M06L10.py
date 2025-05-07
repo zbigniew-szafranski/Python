c1210a2d5d4a9e5c030ca42e4d345f7c8b8b11f7
zbigniew-szafranski <zbigniew.szafranski@gmail.com>
2025-05-07 11:50:15 +0200

    Moduł 6 Lekcja 10 todo

---
+++ M06/solutions/M06L10.py
@@ -0,0 +1,53 @@
+### 🔴 Ćwiczenie
+
+# Napisz program służący do zarządzania listą zadań do wykonania (todolist).
+
+# Program powinien wczytać listę zadań z pliku CSV takiego jak todos.csv, a następnie wyświetlić wszystkie zadania w tabeli, a także podsumowanie ile zadań jest już wykonanych.
+
+# Każde zadanie składa się z id, opisu oraz informacji, czy zadanie zostało już wykonane (znak 'x'), czy jeszcze nie (znak '-').
+
+import csv
+
+PATH = "M06/todos.csv"
+FIELDNAMES = "ID DONE? DESCRIPTION"
+UNDERLINE = "-- ---- --------------"
+COUNT_OF_TASKS = "Liczba wykonanych zadań:"
+
+class Task:
+    def __init__(self, row):
+        self.id = row["id"]
+        self.description = row['desc']
+        self.status = row['done']
+
+    def __str__(self):
+        return f"{self.id:>3} {self.status:^3} {self.description}"
+
+    def __repr__(self):
+        return f"Task(id={self.id!r}, description={self.description!r}, status={self.status!r})"
+
+
+def count_done(tasks):
+    return len([t for t in tasks if t.status == 'x'])
+
+def count_tasks(tasks):
+    return len(tasks)
+
+
+def download_tasks():
+    with open(PATH) as stream:
+        reader = csv.DictReader(stream)
+        tasks = [Task(row) for row in reader]
+        return tasks
+
+def main():
+    tasks_list = download_tasks()
+    print(FIELDNAMES)
+    print(UNDERLINE)
+    for task in tasks_list:
+        print(task)
+    print(COUNT_OF_TASKS, f"{count_done(tasks_list)}/{count_tasks(tasks_list)}")
+
+if __name__ == "__main__":
+    main()
+
+
