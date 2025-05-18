8ad95b3b3433beeffdf2d26ac32152c86715de9b
zbigniew-szafranski <zbigniew.szafranski@gmail.com>
2025-05-18 23:07:18 +0200

    M07L07

--- M07/solutions/M07L05.py
+++ M07/solutions/M07L05.py
@@ -13,15 +13,17 @@ class TodoItem:
         return f'TodoItem(id={self.id}, description={self.description}, done={self.done})'
     def __eq__(self, other):
         return self.id == other.id and self.description == other.description and self.done == other.done
+    def __str__(self):
+        return f'{self.id:<4} | {self.description:<30} | {str(self.done):<5}'
 
 @click.command()
 @click.option('--example', is_flag=True)
 def main(example: bool):
     if example:
         todos = [
-            TodoItem(id=1, description='Zrobij dom', done=False),
-            TodoItem(id=2, description='Zrob kotu', done=True),
-            TodoItem(id=3, description='Zrob piesa', done=False),
+            TodoItem(id=1, description='Zadanie pierwsze', done=False),
+            TodoItem(id=2, description='Zadanie drugie', done=True),
+            TodoItem(id=3, description='Zadanie trzecie', done=False),
         ]
     else:
         todos = []
