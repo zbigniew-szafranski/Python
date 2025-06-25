40e294f51f918124864df09eeb1cfff9b6db5fcc
zbigniew-szafranski <zbigniew.szafranski@gmail.com>
2025-06-25 22:28:24 +0200

    lottery final

---
+++ ćwiczenia/salary.py
@@ -0,0 +1,22 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def count_salary(self):
+        return self.salary
+
+class Manager(Employee):
+    def __init__(self, name, salary, bonus):
+        super().__init__(name, salary)
+        self.bonus = bonus
+
+    def count_salary(self):
+        return self.salary + self.bonus
+
+
+john = Employee('john', 500)
+edek = Manager('edek', 500, 1000)
+
+print(john.count_salary())
+print(edek.count_salary())
