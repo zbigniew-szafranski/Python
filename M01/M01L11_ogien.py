f357b961845760b12d7e51e77d2accd1a38a8722
zbigniew-szafranski <zbigniew.szafranski@gmail.com>
2025-08-05 13:03:32 +0200

    usunięcie folderu z repo

--- M01/M01L11_ogien.py
+++
@@ -1,9 +0,0 @@
-### 🔴 VSCode, Python i... ogień!
-
-### 🔴 Ćwiczenie
-
-# 1. Wybierz wersję Pythona.
-# 2. Uruchom ten program przyciskiem Play.
-# 3. Uruchom ten program poleceniem "run python file in terminal"
-
-print("Praktyczny Python")
