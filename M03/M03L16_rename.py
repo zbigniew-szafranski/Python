2ff34237c8ac7782f220e172c52ab51d1c062817
zbigniew-szafranski <zbigniew.szafranski@gmail.com>
2025-08-05 13:45:49 +0200

    porządki w github

--- M03/M03L16_rename.py
+++
@@ -1,4 +0,0 @@
-### 🔴 Ćwiczenie
-
-# Rozwiń program z M03L12 tak, aby program najpierw wyświetlił jakie nastąpią zmiany plików, następnie poprosił użytkownika o potwierdzenie i dopiero potem dokonał zmiany nazw.
-# W tym celu potrzebujesz listę zmian. Każda zmiana będzie dwuelementową listą zawierającą starą i nową nazwę pliku. Będziesz mieć do czynienie z listą list.
